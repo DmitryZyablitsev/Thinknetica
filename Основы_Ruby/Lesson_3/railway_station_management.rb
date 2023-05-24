84476c2dcc69e971dc634c6bf5aa46fa5e686232
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-05-24 13:48:18 +0500

    Lesson_3.2

--- Основы_Ruby/Lesson_3/railway_station_management.rb
+++ Основы_Ruby/Lesson_3/railway_station_management.rb
@@ -1,24 +1,21 @@
 class Station 
-  attr_accessor :list_of_trains
-  attr_reader :number_of_trains_at_the_station_by_type, :station_name, :list_of_trains
-  def initialize (station_name)
-    @station_name = station_name
-    @list_of_trains = {}
-    @number_of_trains_at_the_station_by_type = [0,0] # первое число будет количество грузовых поездов второе пассажерских 
+  attr_accessor :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []    
   end
 
   def accept_train (train)
-    list_of_trains[train.number] = train
-    train.type == :грузовой ? number_of_trains_at_the_station_by_type[0] +=1 : number_of_trains_at_the_station_by_type[1] +=1
+    trains.push train
   end
 
-
-  def trains_at_the_station_by_type
-    number_of_trains_at_the_station_by_type
+  def trains_by_type(type)
+    trains.select{|train| train.type == type }
   end
 
-  def send_a_train (number)
-    list_of_trains.delete(number.to_sym)
+  def send_a_train (train)
+    trains.delete(train)
   end
 end
 
@@ -50,7 +47,7 @@ class Train
 
   def initialize (number, type, number_of_wagons)
     @number = number.to_sym
-    [:грузовой, :пассажирский].include?(type.to_sym) ? @type = type.to_sym : (abort 'Не корректный тип поезда') # обработку ошибок еще не проходили
+    @type = type.to_sym
     @number_of_wagons = number_of_wagons
     @speed = 0
   end
@@ -63,7 +60,7 @@ class Train
     self.speed = 0
   end
 
-  def attach_wagon
+  def add_wagon
     self.number_of_wagons += 1 if speed == 0
   end
 
@@ -77,7 +74,7 @@ class Train
   end
 
   def move_next_station
-    if route.index(self.current_station) < route.size
+    if route.index(self.current_station) < route.size - 1
       self.current_station = route[route.index(self.current_station) + 1]
     end
   end
@@ -100,34 +97,3 @@ class Train
     end    
   end
 end
-
-# st = Station.new('UK')
-# tr = Train.new('E2', 'грузовой', 60)
-# tr2 = Train.new('B2', 'пассажирский', 32)
-# st.accept_train(tr)
-# st.accept_train(tr2)
-# p st.list_of_trains
-# st.send_a_train('E2')
-# p st.list_of_trains
-
-ro = Route.new('UK', 'EKB')
-ro.add_station(1, 'ZLAT')
-
-
-
-tr = Train.new('E2', 'грузовой', 60)
-# tr.gain_speed(25)
-# p tr.speed
-# tr.brake
-# p tr.speed
-# p tr.number_of_wagons
-# tr.attach_wagon
-# p tr.number_of_wagons
-# tr.unhook_wagon
-# p tr.number_of_wagons
-tr.assign_route(ro)
-p tr.current_station
-tr.move_next_station
-tr.move_next_station
-p tr.show_next_station
-p tr.show_previous_station
