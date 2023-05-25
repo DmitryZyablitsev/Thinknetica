3c0864f1c32885de4ecddffe922ac33ddc9ca3d5
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-05-25 23:43:33 +0500

    Lesson_3.4

--- Основы_Ruby/Lesson_3/railway_station_management.rb
+++ Основы_Ruby/Lesson_3/railway_station_management.rb
@@ -1,54 +1,52 @@
-class Station 
+class Station
   attr_accessor :trains
 
   def initialize(name)
     @name = name
-    @trains = []    
+    @trains = []
   end
 
-  def accept_train (train)
-    trains.push train
+  def accept_train(train)
+    trains << train
   end
 
   def trains_by_type(type)
-    trains.select{|train| train.type == type }
+    trains.select{ |train| train.type == type }
   end
 
-  def send_a_train (train)
+  def send_train(train)
     trains.delete(train)
   end
 end
 
 
 
-
 class Route
-  attr_accessor :route # команда xxx.route может выводить список всех станций по-порядку от начальной до конечной
+  attr_accessor :stations
 
   def initialize (first_station, last_station)
-    @route = [first_station,last_station]
+    @stations = [first_station,last_station]
   end
 
-  def add_station (index, station)
-    route.insert(index, station) if (0..route.size-1).include?(index)
+  def add_station(index, station)
+    stations.insert(index, station) if index > 0 and index < stations.size
   end
 
-  def delete_station (station)
-    route.delete(station)
+  def delete_station(station)
+    stations.delete(station) if station != stations.first and station != stations.last
   end
 end
 
 
 
-
-class Train 
-  attr_accessor :speed, :current_station, :number_of_wagons # speed вернет текущую скорость, метод не стал писать 
+class Train
+  attr_accessor :quantity_wagons, :speed, :current_station_index
   attr_reader :number, :type, :route
 
-  def initialize (number, type, number_of_wagons)
-    @number = number.to_sym
-    @type = type.to_sym
-    @number_of_wagons = number_of_wagons
+  def initialize(number, type, quantity_wagons)
+    @number = number
+    @type = type
+    @quantity_wagons = quantity_wagons
     @speed = 0
   end
 
@@ -61,39 +59,35 @@ class Train
   end
 
   def add_wagon
-    self.number_of_wagons += 1 if speed == 0
+    self.quantity_wagons += 1 if speed == 0
   end
 
   def unhook_wagon
-    self.number_of_wagons -=1 if speed = 0 && number_of_wagons > 0
+    self.quantity_wagons -=1 if speed = 0 && quantity_wagons > 0
   end
 
-  def assign_route(route)
-    @route = route.route
-    self.current_station = self.route.first
-  end
+  def assign_route (route)
+    @route = route
+    @current_station_index = 0
+  end  
 
   def move_next_station
-    if route.index(self.current_station) < route.size - 1
-      self.current_station = route[route.index(self.current_station) + 1]
-    end
+    self.current_station_index += 1 if next_station
   end
 
-  def move_previous_station
-    if route.index(self.current_station) > 0
-      self.current_station = route[route.index(self.current_station) - 1]
-    end
+  def move_previous_station 
+    self.current_station_index -= 1 if previous_station
   end
 
-  def show_next_station
-    route[route.index(self.current_station)+ 1]
+  def next_station
+    route.stations[current_station_index + 1]
   end
 
-  def show_previous_station
-    if route.index(self.current_station) > 0
-    route[route.index(self.current_station) - 1]
-    else
-      nil
-    end    
+  def previous_station
+    current_station_index == 0 ? nil : route.stations[current_station_index - 1] 
   end
+
+  def current_station
+    route.stations[current_station_index]
+  end  
 end
